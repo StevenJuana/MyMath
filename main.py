e44185cbbc8050c2253d93a1e6c36d3edec8f8c3
Steven Juana <sjuana@uci.edu>
2020-08-13 17:42:25 -0700

    Added base and index html

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "this is the initial home page <h1> TEST </h1>"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)

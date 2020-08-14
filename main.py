c7d6da5e0c8b9b7a19f4c7f6d39da2ed058381dc
Steven Juana <sjuana@uci.edu>
2020-08-13 18:12:08 -0700

    Added comments for clarification

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/lessons/")
+def lessons():
+    return render_template("lessons.html")
+
 if __name__ == "__main__":
     app.run(debug=True)

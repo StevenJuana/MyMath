30a50cc4b72e46c31b41fac6f84deff6313ccf8f
Mattthew Grosman <50124151+matthewgrosman@users.noreply.github.com>
2020-08-13 20:32:55 -0700

    Fixed spacing issues

--- main.py
+++ main.py
@@ -2,13 +2,16 @@ from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
     return render_template("index.html")
 
+
 @app.route("/lessons/")
 def lessons():
     return render_template("lessons.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)

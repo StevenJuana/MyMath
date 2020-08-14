5d13195d1b54e0cf5bc02be94f8a1aab9e031387
Mattthew Grosman <50124151+matthewgrosman@users.noreply.github.com>
2020-08-14 01:37:06 -0700

    Changed easy to beginner

--- main.py
+++ main.py
@@ -14,17 +14,9 @@ def lessons():
     return render_template("lessons.html")
 
 
-@app.route("/additionEasy")
-def addition_easy():
-    value1 = randint(0,10)
-    value2 = randint(0,10)
-    answer = value1 + value2
-    return render_template("twoNumEquation.html", heading="Addition - Easy")
-
-
 @app.route("/<problem_type>/<difficulty>/twoNumberProblem")
 def two_number_problem(problem_type: str, difficulty: str):
-    if difficulty == "Easy":
+    if difficulty == "Beginner":
         value1 = randint(0, 10)
         if problem_type == "Subtraction":
             value2 = randint(0, value1-1)
@@ -52,49 +44,6 @@ def two_number_problem(problem_type: str, difficulty: str):
                      "problem_type": problem_type}
 
     return render_template("twoNumEquation.html", question_dict=question_dict)
-
-
-@app.route("/additionIntermediate")
-def addition_intermediate():
-    return render_template("twoNumEquation.html", problem_type="Addition", difficulty="Intermediate")
-
-
-@app.route("/additionAdvanced")
-def addition_advanced():
-    return render_template("twoNumEquation.html", problem_type="Addition", difficulty="Advanced")
-
-
-@app.route("/subtractionEasy")
-def subtraction_easy():
-    return render_template("twoNumEquation.html", problem_type="Subtraction", difficulty="Easy")
-
-
-@app.route("/subtractionIntermediate")
-def subtraction_intermediate():
-    return render_template("twoNumEquation.html", problem_type="Subtraction", difficulty="Intermediate")
-
-
-@app.route("/subtractionAdvanced")
-def subtraction_advanced():
-    return render_template("twoNumEquation.html", problem_type="Subtraction", difficulty="Advanced")
-
-
-@app.route("/multiplicationEasy")
-def multiplication_easy():
-    return render_template("twoNumEquation.html", problem_type="Multiplication", difficulty="Easy")
-
-
-@app.route("/multiplicationIntermediate")
-def multiplication_intermediate():
-    return render_template("twoNumEquation.html", problem_type="Multiplication", difficulty="Intermediate")
-
-
-@app.route("/multiplicationAdvanced")
-def multiplication_advanced():
-    return render_template("twoNumEquation.html", problem_type="Multiplication", difficulty="Advanced")
-
-
-
 
 
 if __name__ == "__main__":

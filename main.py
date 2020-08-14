0f2c9b5b35e664491b2b632e70ae660730f23a35
Steven Juana <sjuana@uci.edu>
2020-08-13 17:05:03 -0700

    test commit

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "this is the initial home page"
+    return "this is the initial home page <h1> TEST </h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)

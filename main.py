7e33ceef1888c3efcbb179b9fc51532ed89d57cd
Mattthew Grosman <50124151+matthewgrosman@users.noreply.github.com>
2020-08-14 20:44:24 -0700

    Added a POST method for single variable

--- main.py
+++ main.py
@@ -163,60 +163,86 @@ def division_problem(difficulty: str):
             else render_template("divisionInterAdvanced.html", question_dict=question_dict)
 
 
-@app.route("/BasicAlgebra/SingleVariableEquation")
+@app.route("/BasicAlgebra/SingleVariableEquation",  methods=['GET', 'POST'])
 def basic_single_var():
     """Generates a single variable equation algebra problem. This can take a few forms:
     value1*x + value2 = value3, value1*x - value2 = value3, value1*x + value3 = -value2*x,
     or value1*x - value3 = value2*x"""
-    problem_type = randint(0, 3)
 
-    if problem_type in [0, 1]:
-        value1 = randint(5, 99)
+    # If the user has just entered an answer submission, check the validity and correctness
+    if request.method == 'POST':
+        # Check if a problem has been asked in this current session... if so, get its details
+        if "currentDict" in session:
+            question_dict = session["currentDict"]
+            given_answer = request.form["answer"]
 
-        while calculations.is_prime(value1):
-            value1 = randint(5, 99)
+            # Check to see if the answer the user submitted is valid, then check its correctness and
+            # give the appropriate error message
+            if given_answer != None and given_answer != "":
+                if given_answer.isnumeric() or (given_answer[0] == "-" and given_answer[1:].isnumeric()):
+                    if int(given_answer) == session["currentDict"]["answer"]:
+                        flash('Correct Answer!')
+                    else:
+                        flash('Incorrect, try again')
+                else:
+                    flash('Invalid Answer, Please Submit a Number')
+            else:
+                flash('Please Type Your Answer Above')
 
-        value2 = randint(5, 99) if problem_type == 0 else randint(5, value1 - 1)
-        value3 = value1 + value2 if problem_type == 0 else value1 - value2
-
-        potential_vars = list(filter(lambda x: (value1 % x == 0), [x for x in range(1, value1)]))
-        answer = potential_vars[randint(0, len(potential_vars) - 1)]
-
-        value1 = int(value1 / answer)
-        sign = "+" if problem_type == 0 else "-"
-
-        #print(value1, " * ", answer, sign, value2, "  = ", value3)
+        # Render the template show the screen shows the correct values
+        return render_template("algebraSingleVariable1.html", question_dict=question_dict) if session["problem_type"] \
+            in [0, 1] else render_template("algebraSingleVariable2.html", question_dict=question_dict)
 
     else:
-        value1 = randint(2, 10)
-        value2 = randint(1, 10) if problem_type == 2 else randint(1, value1 - 1)
+        problem_type = randint(0, 3)
 
-        temp_combination = value1 + value2 if problem_type == 2 else value1 - value2
+        if problem_type in [0, 1]:
+            value1 = randint(5, 99)
 
-        answer = randint(1, 10)
+            while calculations.is_prime(value1):
+                value1 = randint(5, 99)
 
-        value2 = -value2 if problem_type == 2 else value2
-        value3 = answer * temp_combination if problem_type == 2 else -answer * temp_combination
-        answer = -answer if problem_type == 2 else answer
+            value2 = randint(5, 99) if problem_type == 0 else randint(5, value1 - 1)
+            value3 = value1 + value2 if problem_type == 0 else value1 - value2
 
-        sign = "+" if problem_type == 2 else "-"
-        value3 = abs(value3)
+            potential_vars = list(filter(lambda x: (value1 % x == 0), [x for x in range(1, value1)]))
+            answer = potential_vars[randint(0, len(potential_vars) - 1)]
 
-        #print(value1, "x ", value3, " = ", value2, "x", "    x = ", answer)
+            value1 = int(value1 / answer)
+            sign = "+" if problem_type == 0 else "-"
 
-    potential_vars = ["x", "y", "z"]
-    variable = potential_vars[randint(0, len(potential_vars)-1)]
+        else:
+            value1 = randint(2, 10)
+            value2 = randint(1, 10) if problem_type == 2 else randint(1, value1 - 1)
 
-    question_dict = {"value1": value1,
-                     "value2": value2,
-                     "value3": value3,
-                     "answer": answer,
-                     "sign": sign,
-                     "heading": f"Algebra - Single Variable Equations",
-                     "variable": variable}
+            temp_combination = value1 + value2 if problem_type == 2 else value1 - value2
 
-    return render_template("algebraSingleVariable1.html", question_dict=question_dict) if problem_type in [0, 1] \
-        else render_template("algebraSingleVariable2.html", question_dict=question_dict)
+            answer = randint(1, 10)
+
+            value2 = -value2 if problem_type == 2 else value2
+            value3 = answer * temp_combination if problem_type == 2 else -answer * temp_combination
+            answer = -answer if problem_type == 2 else answer
+
+            sign = "+" if problem_type == 2 else "-"
+            value3 = abs(value3)
+
+        potential_vars = ["x", "y", "z"]
+        variable = potential_vars[randint(0, len(potential_vars)-1)]
+
+        question_dict = {"value1": value1,
+                         "value2": value2,
+                         "value3": value3,
+                         "answer": answer,
+                         "sign": sign,
+                         "heading": f"Algebra -\nSingle Variable",
+                         "variable": variable,
+                         "problem_type": problem_type}
+
+        # Save this information in the session dictionary to reference once an answer is entered
+        session["currentDict"] = question_dict
+
+        return render_template("algebraSingleVariable1.html", question_dict=question_dict) if problem_type in [0, 1] \
+            else render_template("algebraSingleVariable2.html", question_dict=question_dict)
 
 
 if __name__ == "__main__":
